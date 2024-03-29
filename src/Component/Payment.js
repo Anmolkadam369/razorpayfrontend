50ce832307ecebdd8420b1d79ae4dbd935fa1b76
anmol <anmolkadam369@gmail.comm>
2024-03-29 17:45:10 +0530

    yes1

--- src/Component/Payment.js
+++ src/Component/Payment.js
@@ -9,17 +9,46 @@ function Payment() {
     price:250,
   });
 
+const initPayment = (data) => {
+  const options = {
+    key: '---------------',
+    amount : data.amount,
+    currency : data.currency,
+    name : book.name,
+    description : "Test Transaction",
+    img : book.img,
+    order_id : data.id,
+    handler : async (response) => {
+      try {
+        const verifyUrl = 'http://localhost:3001/verify';
+        const {data} = await axios.post(verifyUrl, response);
+        console.log("verifyData",data);
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    theme:{
+      color: "#3399cc"
+    },
+  }
+  const rzp1 = new window.Razorpay(options);
+  rzp1.open();
+}
+
   const handlePayment = async () => {
     try {
       const orderUrl = 'http://localhost:3001/orders';
       const {data} = await axios.post(orderUrl, {
         amount: book.price
       });
-      console.log(data)
+      console.log("orderData",data);
+      initPayment(data.data);
     } catch (error) {
       console.log(error)
     }
   }
+
+
   return (
     <div className="payment">
       <div className='book_container'>
